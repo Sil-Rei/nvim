28d3d680495fbdab89b9ffa31be8c66dcb6fda6e
Sil-Rei <silas.reiling@hotmail.de>
2024-12-02 20:16:21 +0100

    rm dashboard added oil main

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -1,14 +1,28 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy=false,
-  config = function()
-    require('oil').setup({
-    default_file_explorer = true,
-    -- default_file_explorer = false,
-    })
-  end
-
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optionale Abhängigkeiten
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"lewis6991/gitsigns.nvim",
+	},
+	lazy = false,
+	config = function()
+		require("oil").setup({
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, bufnr)
+					return name == ".." or name == ".git"
+				end,
+			},
+      columns = {
+        "icon",
+        {
+          "git_status",
+          highlight = "DiagnosticSignHint",
+        },
+      },
+		})
+	end,
 }

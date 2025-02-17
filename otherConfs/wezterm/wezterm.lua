847dbf0e1e2caad32d8d8284fa1ea2bbd3425fe6
Sil-Rei <silas.reiling@hotmail.de>
2025-02-17 16:24:52 +0100

    added wezterm config

--- otherConfs/wezterm/wezterm.lua
+++ otherConfs/wezterm/wezterm.lua
@@ -1,14 +1,10 @@
 local wezterm = require("wezterm")
-
-local config = {}
-
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+local config = wezterm.config_builder()
 
 config.font_size = 15.0
 config.line_height = 1.2
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("MonoLisa Nerd Font")
 
 config.window_decorations = "RESIZE"
 config.initial_rows = 40
@@ -23,4 +19,46 @@ config.colors = require("nordic")
 
 config.send_composed_key_when_left_alt_is_pressed = true
 
+config.keys = {
+	-- Vim-like Pane Navigation (Alt + h/j/k/l)
+	{ key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+
+	-- Pane Splits (Vim-style + CMD)
+	{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "CMD", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+
+	-- Tab Management (CMD)
+	{ key = "t", mods = "CMD", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "CMD|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+
+	-- Tab Switching (CMD + Zahl)
+	{ key = "1", mods = "CMD", action = wezterm.action.ActivateTab(0) },
+	{ key = "2", mods = "CMD", action = wezterm.action.ActivateTab(1) },
+	{ key = "3", mods = "CMD", action = wezterm.action.ActivateTab(2) },
+	{ key = "4", mods = "CMD", action = wezterm.action.ActivateTab(3) },
+	{ key = "5", mods = "CMD", action = wezterm.action.ActivateTab(4) },
+}
+
+smart_splits.apply_to_config(config, {
+	-- the default config is here, if you'd like to use the default keys,
+	-- you can omit this configuration table parameter and just use
+	-- smart_splits.apply_to_config(config)
+
+	-- directional keys to use in order of: left, down, up, right
+	direction_keys = { "h", "j", "k", "l" },
+	-- if you want to use separate direction keys for move vs. resize, you
+	-- can also do this:
+	-- modifier keys to combine with direction_keys
+	modifiers = {
+		move = "ALT", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "CTRL", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+	-- log level to use: info, warn, error
+	log_level = "info",
+})
 return config
+

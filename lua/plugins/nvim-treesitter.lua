2e6cfb46e1c57d75f91194a490616f894ad4da55
Sil-Rei <silas.reiling@hotmail.de>
2026-08-05 10:09:29 +0200

    noice fix

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,8 +3,32 @@ return {
   branch = "main",
   lazy = false, -- main-Branch unterstützt kein Lazy-Loading
   build = ":TSUpdate",
+  cond = function()
+    if vim.fn.has("nvim-0.12") == 1 then
+      return true
+    end
+    vim.schedule(function()
+      vim.notify(
+        "nvim-treesitter (main-Branch) braucht Neovim >= 0.12 – Plugin deaktiviert.\nHier läuft: " .. vim.version().major .. "." .. vim.version().minor,
+        vim.log.levels.WARN
+      )
+    end)
+    return false
+  end,
   config = function()
     local ts = require("nvim-treesitter")
+
+    -- Alter master-Checkout? Dann fehlen setup/install -> klare Ansage statt Stacktrace.
+    if type(ts.install) ~= "function" then
+      vim.schedule(function()
+        vim.notify(
+          "nvim-treesitter ist noch auf dem alten master-Checkout.\nFix: nvim --headless '+Lazy! restore' +qa",
+          vim.log.levels.ERROR
+        )
+      end)
+      return
+    end
+
     ts.setup({})
 
     local ensure_installed = {
